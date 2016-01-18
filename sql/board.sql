6256cff2f930cd0dce32db19d369032362fd461d
Vwan <dopingcat@nate.com>
2016-01-18 10:49:03 +0900

    [move *.sql files] (to sql directory)

--- sql/board.sql
+++ sql/board.sql
@@ -7,14 +7,28 @@ no           NUMBER(8),
 title        VARCHAR2(200) NOT NULL,
 content      VARCHAR2(4000) NOT NULL,
 member_no    NUMBER(8),
+member_name  VARCHAR2(30),
 view_cnt     NUMBER(10),
 reg_date     DATE NOT NULL
 ) ;
 
-
-
 ALTER TABLE board
 ADD ( CONSTRAINT board_no_pk PRIMARY KEY ( no ) );
+
+INSERT INTO board
+     VALUES (board_no_seq.nextval,
+             'title',
+             'nae young',
+             '2',
+			 'wdw',
+			 0,
+             SYSDATE);
+			 
+			
+				
+
+COMMIT;
+
 
 CREATE SEQUENCE board_no_seq
  START WITH     1
@@ -22,3 +36,4 @@ CREATE SEQUENCE board_no_seq
  MAXVALUE       99999999
  NOCACHE
  NOCYCLE;
+ 
