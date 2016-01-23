debd5100f3da9e36248288feccb92334bb4b47c0
801sanae <801sanae@gmail.com>
2016-01-23 14:00:32 +0900

    [impl] merge from kim branch to master

--- src/main/java/com/hanains/wesite/dao/guestbook/GuestBookDao.java
+++ src/main/java/com/hanains/wesite/dao/guestbook/GuestBookDao.java
@@ -1,5 +1,15 @@
 package com.hanains.wesite.dao.guestbook;
 
+import java.util.List;
+
+import com.hanains.wesite.domain.guestbook.GuestBook;
+
 public interface GuestBookDao {
+
+	public List<GuestBook> getList() throws Exception;
+
+	public void insert(GuestBook guestBook) throws Exception;
+
+	public void delete(GuestBook guestBook) throws Exception;
 
 }
--- src/main/java/com/hanains/wesite/service/guestbook/GuestBookService.java
+++ src/main/java/com/hanains/wesite/service/guestbook/GuestBookService.java
@@ -1,5 +1,15 @@
 package com.hanains.wesite.service.guestbook;
 
-public interface GuestBookService {
+import java.util.List;
 
+import com.hanains.wesite.domain.guestbook.GuestBook;
+
+public interface GuestBookService {
+	
+	public List<GuestBook> getList() throws Exception;
+	
+	public void insert(GuestBook guestBook) throws Exception;
+	
+	public void delete(GuestBook guestBook) throws Exception;
+	
 }
