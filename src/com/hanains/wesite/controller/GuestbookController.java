bce71ec394ebc0936b05a1d80ca7fd1109fa3e4e
Choyoungju <dudwn.wh@gmail.com>
2016-01-23 15:05:58 +0900

    no message

--- src/com/hanains/wesite/controller/GuestbookController.java
+++ src/com/hanains/wesite/controller/GuestbookController.java
@@ -5,12 +5,14 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.hanains.wesite.service.GuestBookService;
 import com.hanains.wesite.vo.GuestBookVo;
 
 @Controller
+@RequestMapping("/guestbook")
 public class GuestbookController {
 
 	@Autowired
@@ -22,7 +24,27 @@ public class GuestbookController {
 		model.addAttribute("list", list);
 		
 		
-		return "/WEB-INF/views/index.jsp";
+		return "/guestbook/listsp";
+	}
+	
+	
+	@RequestMapping("/insert")
+	public String insert(@ModelAttribute GuestBookVo vo) {
+		
+		if(vo.getMessage().trim().length()==0 ||vo.getName().trim().length()==0 || vo.getPassword().trim().length()==0){
+			return "redirect:/guestbook?result=fail";
+		}
+		
+		guestBookService.write(vo);
+		return "redirect:/guestbook/";
+		
+	}
+	
+	@RequestMapping("/delete")
+	public String delte(@ModelAttribute GuestBookVo vo){
+		
+		guestBookService.delete(vo);
+		return "redirect:/guestbook/";
 	}
 	
 	
