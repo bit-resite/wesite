0df705361a9ad28ba07dc718b4b0f75a90d797c1
801sanae <801sanae@gmail.com>
2016-01-23 15:21:48 +0900

    [imp com.hanains.wesite.*.board.*](not yet

--- src/main/java/com/hanains/wesite/controller/board/BoardController.java
+++ src/main/java/com/hanains/wesite/controller/board/BoardController.java
@@ -1,12 +1,18 @@
 package com.hanains.wesite.controller.board;
 
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.hanains.wesite.domain.board.Board;
 import com.hanains.wesite.service.board.BoardService;
 
 @Controller
+@RequestMapping("/board")
 public class BoardController {
 	
 	@Autowired
@@ -16,10 +22,13 @@ public class BoardController {
 		System.out.println(this.getClass());
 	}
 	
-	@RequestMapping()
-	public String getList(){
+	@RequestMapping("/list")
+	public String list(HttpServletRequest request) throws Exception{
+		List<Board> list = boardService.getList();
+		request.setAttribute("list", list);
 		
-		return "";
+		return "/board/list";
 	}
+	
 	
 }
--- src/main/java/com/hanains/wesite/service/board/BoardService.java
+++ src/main/java/com/hanains/wesite/service/board/BoardService.java
@@ -1,5 +1,11 @@
 package com.hanains.wesite.service.board;
 
+import java.util.List;
+
+import com.hanains.wesite.domain.board.Board;
+
 public interface BoardService {
+
+	public List<Board> getList() throws Exception;
 
 }
